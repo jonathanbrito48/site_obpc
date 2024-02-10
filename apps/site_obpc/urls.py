02996dfda1dfb4df38d5dbc176780e2a5539ac84
Jonathan Brito <117831426+jonathanbrito48@users.noreply.github.com>
2024-02-10 12:08:31 -0300

    site-obpc

--- apps/site_obpc/urls.py
+++ apps/site_obpc/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from apps.site_obpc.views import index
+from apps.site_obpc.views import index,pastores,umasbrac,ufebrac
 
 urlpatterns = [
     path('', index,name='index'),
+    path('pastores',pastores,name='pastores'),
+    path('umasbrac',umasbrac,name='umasbrac'),
+    path('ufebrac',ufebrac,name='ufebrac')
 ]
--- apps/site_obpc/views.py
+++ apps/site_obpc/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'site/index.html')
+
+def pastores(request):
+    return render(request,'site/pastores.html')
+
+def umasbrac(request):
+    return render(request,'site/umasbrac.html')
+
+def ufebrac(request):
+    return render(request,'site/ufebrac.html')
