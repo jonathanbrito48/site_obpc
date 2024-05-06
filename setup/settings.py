c9c106c01a440a814fcddb7c64f5a8e26c6352c2
Jonathan Brito <117831426+jonathanbrito48@users.noreply.github.com>
2024-05-06 10:35:38 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   .gitignore
    	modified:   setup/__pycache__/settings.cpython-311.pyc
    	modified:   setup/settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-33%2aqh%jqg@=$=#m@o5n0hgv-#-e6fhmio#zwh-r#7to79ad3'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
