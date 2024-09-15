aad09914d0a1a07e68a4eeb1dde7da84ad68c353
Jonathan Brito <117831426+jonathanbrito48@users.noreply.github.com>
2024-09-15 12:48:52 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   setup/settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['104.154.120.96']
 
 # Application definition
 
