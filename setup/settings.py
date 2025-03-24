b4e7cdd747a7283d23270ecb0a213abe909b597b
jonathanbrito48 <jonathanbrito48@gmail.comcls>
2025-03-24 13:19:13 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   setup/settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY =  str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','34.121.153.167']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','34.121.153.167','site']
 
 # Application definition
 
