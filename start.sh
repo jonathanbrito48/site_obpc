5b49c7600fb70178592aff6ff41118d7380ef399
jonathanbrito48 <jonathanbrito48@gmail.comcls>
2025-03-24 12:10:23 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,16 @@
 crond
 
 # Inicia o servidor Gunicorn para o Django
-exec gunicorn setup.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn setup.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 2 \
+  --timeout 300 \
+  --worker-class gthread \
+  --threads 3 \
+  --graceful-timeout 30 \
+  --keepalive 5 \
+  --max-requests 1000 \
+  --max-requests-jitter 50 \
+  --log-level debug \
+  --access-logfile - \
+  --error-logfile -
